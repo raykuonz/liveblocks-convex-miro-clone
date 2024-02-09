34013c66e1ab2c11fdae1fe938e493d2a28c237f
Ray Kuo <enihu@gmail.com>
2024-02-09 13:32:19 +1300

    Setup Convex backend and Clerk auth

---
+++ components/auth/loading.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const Loading = () => {
+  return (
+    <div className="h-full w-full flex flex-col gap-y-4 justify-center items-center">
+        <Image
+            src="/logo.svg"
+            alt="Logo"
+            width={120}
+            height={120}
+            className="animate-pulse duration-700"
+        />
+    </div>
+  )
+}
+
+export default Loading;
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://amazed-foal-71.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
