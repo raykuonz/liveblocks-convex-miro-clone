8a6bc20a8ac843238692e3128f66f93b458f8977
Ray Kuo <enihu@gmail.com>
2024-02-13 13:14:00 +1300

    Added board action to copy link, rename and delete board

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -8,6 +8,8 @@ import Overlay from "./overlay";
 import { useAuth } from "@clerk/nextjs";
 import Footer from "./footer";
 import { Skeleton } from "@/components/ui/skeleton";
+import Actions from "@/components/actions";
+import { MoreHorizontal } from "lucide-react";
 
 interface BoardCardProps {
   id: string;
@@ -49,6 +51,19 @@ const BoardCard = ({
             className="object-fit"
           />
           <Overlay />
+          <Actions
+            id={id}
+            title={title}
+            side="right"
+          >
+            <button
+              className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none"
+            >
+              <MoreHorizontal
+                className="text-white opacity-75 hover:opacity-100 transition-opacity"
+              />
+            </button>
+          </Actions>
         </div>
 
         <Footer
